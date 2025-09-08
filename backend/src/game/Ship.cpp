a1f8386ff9a6140b0640b6e10febe83f992b325d
4monika <vanskov14@mail.ru>
2025-09-08 19:56:15 +0300

    realization of functions IsHit and IsDestroyed in class Ship

--- backend/src/game/Ship.cpp
+++ backend/src/game/Ship.cpp
@@ -2,10 +2,33 @@
 
 namespace game {
 
-bool Ship::IsHit(const Coordinates& coord) noexcept {}
+/**
+ * @4monika Реализация
+ */
+bool Ship::IsHit(const Coordinates& coord) noexcept {
+    for (size_t i = 1; i < positions_.size(); i++) {
+        if (coord == positions_[i]) {
+            hits_[i] = true;
+            return true;
+        }
+    }
+    return false;
+}
 
-bool Ship::IsDestroyed() const noexcept {}
-
+/**
+ * @4monika Реализация
+ */
+bool Ship::IsDestroyed() const noexcept {
+    for (size_t i = 1; i < hits_.size(); i++) {
+        if (!hits_[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+/**
+ * @4monika Реализация
+ */
 const std::vector<Ship::Coordinates>& Ship::GetPositions() const noexcept {
     return positions_;
 }
