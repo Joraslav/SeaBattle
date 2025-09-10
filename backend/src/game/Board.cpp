b96f1117192abaa3731111cb093c3a9d745eddb1
4monika <vanskov14@mail.ru>
2025-09-10 14:49:02 +0300

    Realized Shoot method

--- backend/src/game/Board.cpp
+++ backend/src/game/Board.cpp
@@ -2,6 +2,12 @@
 
 namespace board {
 
-bool Board::Shoot(const Coordinates& coord) const {}
+bool Board::Shoot(const Coordinates& coord) const {
+    if (cells_.find(coord) == cells_.end()) return false;
+    for (const ShipPtr& ship : ships_) {
+        bool hit = ship->IsHit(coord);
+        if (hit) return hit;
+    }
+}
 
 }  // namespace board
