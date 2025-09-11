81798c70ce4144812d9991f2922d666258091a71
Joraslav <yar.mozg2002@gmail.com>
2025-09-11 20:40:09 +0300

    Begin Player

--- backend/src/game/Board.cpp
+++ backend/src/game/Board.cpp
@@ -4,36 +4,10 @@ namespace board {
 
 // ------ Public методы ------
 
-// Один из вариантов реализации
-/*
-void Board::AddShip(ShipPtr ship, const Coordinates& coord) {
-    // Проверка на максимальное количество кораблей
-    if (ships_.size() >= MAX_SHIPS) return;
 
-    // Проверка на пересечение с существующими кораблями
-    const auto& new_positions = ship->GetPositions();
-    for (const auto& s : ships_) {
-        for (const auto& pos : s->GetPositions()) {
-            for (const auto& new_pos : new_positions) {
-                if (pos == new_pos) return; // Пересечение, не добавляем
-            }
-        }
-    }
-
-    // Добавляем корабль
-    ships_.push_back(std::move(ship));
-
-    // Обновляем состояние клеток
-    for (const auto& pos : new_positions) {
-        Get(pos) = StateCell::SHIP;
-    }
-}
-*/
-
-//
 Board::StateCell Board::Shoot(const Coordinates& coord) {
-    // Заглушка
-    return StateCell::EMPTY;
+    StateCell& cell_state = Get(coord);
+
 }
 
 // ------ Private методы ------
